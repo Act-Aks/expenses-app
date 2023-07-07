4e41184474517ea9ed2086f19562fca36a1533cc
Act-Aks <akashsrivastava.git@gmail.com>
2023-07-07 20:12:09 +0530

    setup module struct

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,33 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['nativewind/babel', 'react-native-reanimated/plugin'],
+    plugins: [
+      'nativewind/babel',
+      'react-native-reanimated/plugin',
+      [
+        'module-resolver',
+        {
+          root: ['.'],
+          extensions: [
+            '.js',
+            '.jsx',
+            '.ts',
+            '.tsx',
+            '.android.js',
+            '.android.tsx',
+            '.ios.js',
+            '.ios.tsx',
+          ],
+          alias: {
+            '@': './src',
+            '@assets': './assets',
+            '@components': './src/components',
+            '@hooks': './src/hooks',
+            '@infrastructure': './src/infrastructure',
+            '@screens': './src/screens',
+          },
+        },
+      ],
+    ],
   };
 };
